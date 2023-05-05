6ba856cd616069afe478cbf9522d4192a7be2e6d
Mendel E <mendelmaleh@gmail.com>
2023-05-05 17:31:58 -0400

    Simplify, special case resume

--- main.go
+++ main.go
@@ -4,89 +4,82 @@ import (
 	"bytes"
 	"fmt"
 	"html/template"
+	"io"
 	"io/fs"
 	"log"
 	"os"
 	"path/filepath"
-	"sync"
 
 	"github.com/gomarkdown/markdown"
-	"github.com/gomarkdown/markdown/html"
-	"github.com/gomarkdown/markdown/parser"
 )
 
+type Page struct {
+	Name    string
+	Content template.HTML
+}
+
 var (
-	mdtohtml chan (string)
-	wg       sync.WaitGroup
+	tmpl = template.Must(template.ParseFiles("base.tmpl"))
+
+	srcdir = "src"
+	outdir = "out"
 )
 
 func walkfunc(path string, info fs.FileInfo, err error) error {
+	if err != nil || info.IsDir() {
+		return err
+	}
+
+	// prepare path
+	rp, err := filepath.Rel(srcdir, path)
 	if err != nil {
 		return err
 	}
 
-	if info.IsDir() {
-		return nil
+	dest := filepath.Join(outdir, rp)
+	if err := os.MkdirAll(filepath.Dir(dest), os.ModePerm); err != nil {
+		return err
 	}
 
-	fmt.Println(path)
-
-	wg.Add(1)
+	// process file
 	if filepath.Ext(path) == ".md" {
-		mdtohtml <- path
-	}
-
-	return nil
-}
-
-func main() {
-	if err := func() error {
-		// templates
-		tmpl := template.Must(template.ParseFiles("base.tmpl"))
-
-		// markdown
-		mdtohtml = make(chan string)
-		go func() {
-			for path := range mdtohtml {
-				md, err := os.ReadFile(path)
-				if err != nil {
-					log.Fatal(err)
-				}
-
-				p := parser.NewWithExtensions(parser.CommonExtensions)
-				r := html.NewRenderer(html.RendererOptions{
-					Flags: html.CommonFlags,
-				})
-
-				tree := p.Parse(md)
-				// ast.Print(os.Stdout, tree)
-				doc := markdown.Render(tree, r)
-
-				// replace "src" with "out" and ".md" with ".html"
-				path = "out" + path[3:len(path)-3] + ".html"
-
-				// TODO: mkdirall
-				f, err := os.Create(path)
-				if err != nil {
-					log.Fatal(err)
-				}
-
-				tmpl.ExecuteTemplate(f, "base", template.HTML(
-					bytes.ReplaceAll(doc, []byte("\n"), []byte("\n\t\t")),
-				))
-
-				wg.Done()
-			}
-		}()
-
-		if err := filepath.Walk("src", walkfunc); err != nil {
+		doc, err := os.ReadFile(path)
+		if err != nil {
 			return err
 		}
 
-		wg.Wait()
+		doc = markdown.ToHTML(doc, nil, nil)
+		dest = dest[:len(dest)-2] + "html"
 
-		return nil
-	}(); err != nil {
+		f, err := os.Create(dest)
+		if err != nil {
+			return err
+		}
+
+		tmpl.ExecuteTemplate(f, "base", Page{Name: filepath.Base(path), Content: template.HTML(
+			bytes.ReplaceAll(doc, []byte("\n"), []byte("\n\t\t")),
+		)})
+	} else {
+		src, err := os.Open(path)
+		if err != nil {
+			return err
+		}
+
+		dst, err := os.Create(dest)
+		if err != nil {
+			return err
+		}
+
+		_, err = io.Copy(dst, src)
+	}
+
+	fmt.Println(path, "->", dest)
+
+	return err
+}
+
+func main() {
+	if err := filepath.Walk("src", walkfunc); err != nil {
 		log.Fatal(err)
 	}
 }
